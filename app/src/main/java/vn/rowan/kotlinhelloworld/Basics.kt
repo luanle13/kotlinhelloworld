91d341c5be1df48d7292cf2fcb0f0073fbf225e2
luanle13 <thanhluan130201@gmail.com>
2025-01-06 21:49:12 +0700

    day2: int, long, short, byte and specifying the type manually

--- app/src/main/java/vn/rowan/kotlinhelloworld/Basics.kt
+++ app/src/main/java/vn/rowan/kotlinhelloworld/Basics.kt
@@ -1,10 +1,8 @@
 package vn.rowan.kotlinhelloworld
 
 fun main() {
-    val constantNumber = 13
-    var mutableString = "Le Thanh Luan"
-    println(constantNumber)
-    println(mutableString)
-    mutableString = "Rowan Lee"
-    println(mutableString)
+    var myAge : Byte = 24
+    println(myAge)
+    myAge = 25
+    println(myAge)
 }
