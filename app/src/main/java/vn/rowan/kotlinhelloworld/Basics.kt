0c08d4143fd322dacbdbb55ea20b25b1df0b51cf
luanle13 <thanhluan130201@gmail.com>
2025-01-07 12:52:46 +0700

    quiz4: coding exercise - understanding booleans in kotlin

--- app/src/main/java/vn/rowan/kotlinhelloworld/Basics.kt
+++ app/src/main/java/vn/rowan/kotlinhelloworld/Basics.kt
@@ -2,10 +2,21 @@ package vn.rowan.kotlinhelloworld
 
 fun main() {
     // Day 2: Booleans true false negation
+//    val myTrue: Boolean = true
+//    val myFalse: Boolean = false
+//
+//    println(myTrue || myFalse)
+//    println(myTrue && myFalse)
+//    println(!myTrue)
+
+    // Quiz 4: Coding Exercise - Understanding Booleans in Kotlin
+    // Step 1: Declare Boolean variables
     val myTrue: Boolean = true
     val myFalse: Boolean = false
-
-    println(myTrue || myFalse)
-    println(myTrue && myFalse)
-    println(!myTrue)
+    // Step 2: Logical OR Operation
+    println(myTrue || myFalse) // This will be true
+    // Step 3: Logical AND Operation
+    println(myTrue && myFalse) // This will be false
+    // Step 4: Logical NOT Operation
+    println(!myTrue) // This will be false
 }
