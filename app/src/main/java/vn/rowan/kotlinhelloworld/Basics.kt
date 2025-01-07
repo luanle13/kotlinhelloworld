2152a4ea3ca06750107ece04f75e917e9575582c
luanle13 <thanhluan130201@gmail.com>
2025-01-07 12:54:19 +0700

    remove: old lesson

--- app/src/main/java/vn/rowan/kotlinhelloworld/Basics.kt
+++ app/src/main/java/vn/rowan/kotlinhelloworld/Basics.kt
@@ -1,7 +1,7 @@
 package vn.rowan.kotlinhelloworld
 
 fun main() {
-    // Day 2: Booleans true false negation
+//    // Day 2: Booleans true false negation
 //    val myTrue: Boolean = true
 //    val myFalse: Boolean = false
 //
@@ -9,14 +9,16 @@ fun main() {
 //    println(myTrue && myFalse)
 //    println(!myTrue)
 
-    // Quiz 4: Coding Exercise - Understanding Booleans in Kotlin
-    // Step 1: Declare Boolean variables
-    val myTrue: Boolean = true
-    val myFalse: Boolean = false
-    // Step 2: Logical OR Operation
-    println(myTrue || myFalse) // This will be true
-    // Step 3: Logical AND Operation
-    println(myTrue && myFalse) // This will be false
-    // Step 4: Logical NOT Operation
-    println(!myTrue) // This will be false
+//    // Quiz 4: Coding Exercise - Understanding Booleans in Kotlin
+//    // Step 1: Declare Boolean variables
+//    val myTrue: Boolean = true
+//    val myFalse: Boolean = false
+//    // Step 2: Logical OR Operation
+//    println(myTrue || myFalse) // This will be true
+//    // Step 3: Logical AND Operation
+//    println(myTrue && myFalse) // This will be false
+//    // Step 4: Logical NOT Operation
+//    println(!myTrue) // This will be false
+
+
 }
