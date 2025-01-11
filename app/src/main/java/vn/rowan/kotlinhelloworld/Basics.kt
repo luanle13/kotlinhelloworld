01bc30300abbbd3c52b1d9da1509f102b1823cf3
luanle13 <thanhluan130201@gmail.com>
2025-01-12 01:24:38 +0700

    day2: while loops with counters

--- app/src/main/java/vn/rowan/kotlinhelloworld/Basics.kt
+++ app/src/main/java/vn/rowan/kotlinhelloworld/Basics.kt
@@ -100,14 +100,23 @@ fun main() {
 //    }
     //endregion
     //region Quiz 5: Coding Exercise - Exploring "if-else if-else" in Kotlin
-    print("Enter your age as a whole number: ")
-    val age = readln().toInt()
-    if (age in 18..39) {
-        println("You can enter the club.")
-    } else if (age > 40) {
-        println("You cannot go into the club, please go home")
-    } else {
-        println("Age not verified. Please contact support.")
+//    print("Enter your age as a whole number: ")
+//    val age = readln().toInt()
+//    if (age in 18..39) {
+//        println("You can enter the club.")
+//    } else if (age > 40) {
+//        println("You cannot go into the club, please go home")
+//    } else {
+//        println("Age not verified. Please contact support.")
+//    }
+    //endregion
+    //region Day 2: While loops with counters
+    var count = 0
+    while (count < 3) {
+        // so long this content will be executed
+        println("Count is $count")
+        count++ // same as count = count + 1
     }
+    println("Loop is done!")
     //endregion
 }
