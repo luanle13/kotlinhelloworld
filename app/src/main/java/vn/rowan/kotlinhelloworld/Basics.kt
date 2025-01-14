41a05158578be1781672550051cbd570d84106c3
luanle13 <thanhluan130201@gmail.com>
2025-01-14 17:14:33 +0700

    quiz8: sugar spoon with grammar

--- app/src/main/java/vn/rowan/kotlinhelloworld/Basics.kt
+++ app/src/main/java/vn/rowan/kotlinhelloworld/Basics.kt
@@ -143,6 +143,8 @@ fun main() {
     makeCoffee(1)
     makeCoffee(3)
     makeCoffee(30)
+    makeCoffee(-1)
+    makeCoffee(0)
     //endregion
 }
 
@@ -153,5 +155,12 @@ fun makeCoffee(sugarCount: Int) {
     println("Heat water to 90C")
     println("Add water")
     println("Push hot water through the grinded beans")
-    println("Coffee with $sugarCount spoons of sugar")
+
+    if (sugarCount == 1) {
+        println("Coffee with 1 spoon of sugar")
+    } else if (sugarCount > 1) {
+        println("Coffee with $sugarCount spoons of sugar")
+    } else {
+        println("Coffee with no sugar")
+    }
 }
