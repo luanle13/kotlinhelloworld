98b20f119cb700bde9cb1f35defb31c09d70fe88
luanle13 <thanhluan130201@gmail.com>
2025-01-18 16:42:10 +0700

    quiz10: coding exercise - user input arithmetic in kotlin

--- app/src/main/java/vn/rowan/kotlinhelloworld/Basics.kt
+++ app/src/main/java/vn/rowan/kotlinhelloworld/Basics.kt
@@ -279,3 +279,18 @@ package vn.rowan.kotlinhelloworld
 //    }
 //}
 //endregion
+
+//region Quiz 10: Coding Exercise - User Input Arithmetic in Kotlin
+fun main() {
+    println("Please enter the first number: ")
+    val firstNumber: Int = readln().toInt()
+    println("Please enter the second number: ")
+    val secondNumber: Int = readln().toInt()
+    val result = add(firstNumber, secondNumber)
+    println("The result of $firstNumber + $secondNumber is: $result")
+}
+
+fun add(firstNumber: Int, secondNumber: Int): Int {
+    return firstNumber + secondNumber
+}
+//endregion
